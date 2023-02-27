e3c9abbb1e2a9be37d0163019441a8cedd78dd9c
chenxuehai@alltuu.com <cxh741@qq.com>
2023-02-27 15:44:15 +0800

    feat: useMouseInElement取值

--- components/contrastImg.vue
+++ components/contrastImg.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-// TODO vueuse的useMouseInElement有bug似乎，handleOutside为false时，仍然会触发监听，抽空看看是什么原因
 const target = ref(null)
-const { x, isOutside } = useMouseInElement(target, { handleOutside: false })
-const { width: windowWidth } = useWindowSize()
-const afterContainerWidth = computed(() => {
-  // if (isOutside.value) return 0
-  return windowWidth.value - x.value
+const { elementX } = useMouseInElement(target, { handleOutside: false })
+const { width } = useWindowSize()
+const windowWidth = computed(() => {
+  return `${width.value}px`
 })
-watch(isOutside, () => {
-  console.log('isOutside', isOutside.value)
+const afterContainerWidth = computed(() => {
+  return `${width.value - elementX.value}px`
 })
 </script>
 
 <template>
-  <div ref="target" w-screen h-screen relative>
-    <div class="before" w-screen h-screen bg-center bg-cover bg-no-repeat />
+  <div ref="target" width="100%" h-screen relative>
+    <div class="before" width="100%" h-screen bg-center bg-cover bg-no-repeat />
     <div class="after-container" absolute top-0 right-0 overflow-hidden z-3 h-screen>
       <div class="after-bg" absolute top-0 right-0 h-screen bg-center bg-cover bg-no-repeat />
       <span />
@@ -27,10 +25,10 @@ watch(isOutside, () => {
   background-image: url('../assets/before.jpeg');
 }
 .after-container {
-  width: v-bind(`${afterContainerWidth}px`);
+  width: v-bind(afterContainerWidth);
 }
 .after-bg {
   background-image: url('../assets/after.jpeg');
-  width: v-bind(`${windowWidth}px`);
+  width: v-bind(windowWidth);
 }
 </style>
